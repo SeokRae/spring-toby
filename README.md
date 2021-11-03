c74e7fdd7fd9d81bce9c23fd311e34b244eb8ab6
seokrae <kslbsh@gmail.com>
2021-11-03 23:24:30 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Spring Toby
+
+## Table of Contents
+
+1. 오브젝트와 의존관계
---
+++ chapter1/src/main/java/com/example/chapter1/ChapterOneApplication.java
@@ -0,0 +1,11 @@
+package com.example.chapter1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChapterOneApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChapterOneApplication.class, args);
+    }
+}
