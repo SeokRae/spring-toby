5a221e8159e8577ed9bd51d9a9ab4f43b32582b1
seokrae <kslbsh@gmail.com>
2021-11-13 01:00:22 +0900

    spring-toby-3 [Spring] 2 초난감 DAO (2.3)

---
+++ chapter2/src/main/java/com/example/chapter2/dao/UserDao.java
@@ -0,0 +1,72 @@
+package com.example.chapter2.dao;
+
+import com.example.chapter2.domain.User;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import javax.sql.DataSource;
+import java.sql.*;
+
+public class UserDao {
+
+    private final DataSource dataSource;
+
+    public UserDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void add(User user) throws SQLException {
+        try (
+                Connection c = dataSource.getConnection();
+                PreparedStatement ps = c.prepareStatement("INSERT INTO USERS(id, name, password) values (?, ?, ?)")
+        ) {
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getPassword());
+
+            ps.executeUpdate();
+        }
+    }
+
+    public User get(String id) throws SQLException {
+        try (
+                Connection c = dataSource.getConnection();
+                PreparedStatement ps = c.prepareStatement("SELECT * FROM USERS WHERE id = ?")
+        ) {
+            ps.setString(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            User user = null;
+
+            if(rs.next()) {
+                user.setId(rs.getString("id"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
+            }
+
+            if(user == null) {
+                throw new EmptyResultDataAccessException(1);
+            }
+            return user;
+        }
+    }
+
+    public int getCount() throws SQLException {
+        try (
+                Connection c = dataSource.getConnection();
+                PreparedStatement ps = c.prepareStatement("SELECT COUNT(*) FROM USERS");
+        ) {
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        }
+    }
+
+    public void truncateTable() throws SQLException {
+        try (
+                Connection c = dataSource.getConnection();
+                Statement s = c.createStatement()
+        ) {
+            s.executeUpdate("TRUNCATE TABLE USERS");
+        }
+    }
+}
---
+++ chapter2/src/test/java/com/example/chapter2/dao/UserDaoTest.java
@@ -0,0 +1,87 @@
+package com.example.chapter2.dao;
+
+import com.example.chapter2.config.DataSourceConfig;
+import com.example.chapter2.domain.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {DataSourceConfig.class})
+class UserDaoTest {
+
+    @Autowired
+    private DataSource dataSource;
+    private UserDao userDao;
+    private User user1;
+    private User user2;
+    private User user3;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        userDao = new UserDao(dataSource);
+        userDao.truncateTable();
+
+        user1 = new User("user_0", "user_name_0", "1234");
+        user2 = new User("user_1", "user_name_1", "1234");
+        user3 = new User("user_2", "user_name_2", "1234");
+    }
+
+    @DisplayName("테이블 데이터 등록 및 조회 테스트")
+    @Test
+    void testCase1() throws SQLException {
+        assertThat(userDao.getCount()).isZero();
+
+        userDao.add(user1);
+        userDao.add(user2);
+        assertThat(userDao.getCount()).isEqualTo(2);
+
+        User expected1 = userDao.get(user1.getId());
+        assertThat(user1.getName()).isEqualTo(expected1.getName());
+
+        User expected2 = userDao.get(user2.getId());
+        assertThat(user2.getName()).isEqualTo(expected2.getName());
+    }
+
+    @DisplayName("테이블 등록 카운트 테스트")
+    @Test
+    void testCase2() throws SQLException {
+        assertThat(userDao.getCount()).isZero();
+
+        userDao.add(user1);
+        assertThat(userDao.getCount()).isOne();
+
+        userDao.add(user2);
+        assertThat(userDao.getCount()).isEqualTo(2);
+
+        userDao.add(user3);
+        assertThat(userDao.getCount()).isEqualTo(3);
+
+        User actual = userDao.get("user_1");
+
+        assertThat(actual.getName()).isEqualTo("user_name_1");
+    }
+
+    @DisplayName("데이터 액세스 예외 테스트")
+    @Test
+    void testCase3() throws SQLException {
+        assertThat(userDao.getCount()).isZero();
+
+        userDao.add(user1);
+
+        assertThatExceptionOfType(EmptyResultDataAccessException.class)
+                .isThrownBy(() -> userDao.get("111"));
+    }
+}
