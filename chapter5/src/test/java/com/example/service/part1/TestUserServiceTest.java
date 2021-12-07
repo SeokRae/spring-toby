96acfe178606038c0773603b656b4245520c8abb
seokrae <kslbsh@gmail.com>
2021-12-07 12:31:12 +0900

    feat: TestUserService 테스트 클래스 분리

--- chapter5/src/test/java/com/example/service/part1/TestUserServiceTest.java
+++ chapter5/src/test/java/com/example/service/part1/TestUserServiceTest.java
@@ -3,6 +3,7 @@ package com.example.service.part1;
 import com.example.service.config.DataSourceConfig;
 import com.example.service.domain.Level;
 import com.example.service.domain.User;
+import com.example.service.exception.TestUserServiceException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 import java.util.Arrays;
@@ -19,6 +21,7 @@ import java.util.List;
 import static com.example.service.part1.UserService.MIN_LOGCOUNT_FOR_SILVER;
 import static com.example.service.part1.UserService.MIN_RECCOMEND_FOR_GOLD;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {
@@ -26,10 +29,9 @@ import static org.assertj.core.api.Assertions.assertThat;
         UserDaoJdbc.class,
         UserService.class
 })
-class UserServiceTest {
+class TestUserServiceTest {
 
-    @Autowired
-    private UserService userService;
+    private PlatformTransactionManager transactionManager;
 
     @Autowired
     private UserDao userDao;
@@ -38,12 +40,11 @@ class UserServiceTest {
     private DataSource dataSource;
 
     private List<User> users;
+    private UserService testUserService;
 
     @BeforeEach
     void setUp() {
-        userService.setUserDao(userDao);
-        userService.setTransactionManager(new DataSourceTransactionManager(dataSource));
-
+        transactionManager = new DataSourceTransactionManager(dataSource);
         userDao.deleteAll();
         users = Arrays.asList(
                 new User("user1", "username1", "u1", Level.BASIC, MIN_LOGCOUNT_FOR_SILVER - 1, 0)
@@ -54,65 +55,22 @@ class UserServiceTest {
         );
     }
 
-    @DisplayName("빈 주입 테스트")
+    @DisplayName("트랜잭션을 검증하기 위한 테스트")
     @Test
-    void bean_di_expected_success() {
-        assertThat(this.userService).isNotNull();
-    }
+    void upgradeAllOrNothing() {
 
-    @DisplayName("사용자 레벨 업그레이드 테스트")
-    @Test
-    void user_level_update_expected_success() {
+        testUserService = new TestUserService(users.get(3).getId());
+        testUserService.setUserDao(this.userDao);
+        testUserService.setTransactionManager(transactionManager);
+
         for (User user : users) {
             userDao.add(user);
         }
 
-        userService.upgradeLevels();
+        assertThatExceptionOfType(TestUserServiceException.class)
+                .isThrownBy(testUserService::upgradeLevels);
 
-        checkLevel(users.get(0), Level.BASIC);
-        checkLevel(users.get(1), Level.SILVER);
-        checkLevel(users.get(2), Level.SILVER);
-        checkLevel(users.get(3), Level.GOLD);
-        checkLevel(users.get(4), Level.GOLD);
-    }
-
-    private void checkLevel(User user, Level expectedLevel) {
-        User upgradeUser = userDao.get(user.getId());
-        assertThat(upgradeUser.getLevel()).isEqualTo(expectedLevel);
-    }
-
-    @DisplayName("Level 필드 저장 확인 테스트")
-    @Test
-    void check_level_expected_success_field_settings() {
-
-        User userWithLevel = users.get(4);
-        User userWithOutLevel = users.get(0);
-        userWithOutLevel.setLevel(null);
-
-        userService.add(userWithLevel);
-        userService.add(userWithOutLevel);
-
-        User userWithLevelRead = userDao.get(userWithLevel.getId());
-        User userWithOutLevelRead = userDao.get(userWithOutLevel.getId());
-
-        assertThat(userWithLevelRead.getLevel()).isEqualTo(userWithLevel.getLevel());
-        assertThat(userWithOutLevelRead.getLevel()).isEqualTo(userWithOutLevel.getLevel());
-    }
-
-    @DisplayName("Level 필드 개선 테스트")
-    @Test
-    void upgrade_levels() {
-        for (User user : users) {
-            userDao.add(user);
-        }
-
-        userService.upgradeLevels();
-
-        checkLevelUpgraded(users.get(0), false);
-        checkLevelUpgraded(users.get(1), true);
-        checkLevelUpgraded(users.get(2), false);
-        checkLevelUpgraded(users.get(3), true);
-        checkLevelUpgraded(users.get(4), false);
+        checkLevelUpgraded(users.get(1), false);
     }
 
     private void checkLevelUpgraded(User user, boolean upgraded) {
